08d8ced70f9058a338c3753f85c90d63a74ebb0f
rajagurusk <rajagurusivakumar1@gmail.com>
2025-08-02 18:05:08 +0530

    updated login and home

--- IDT/Controllers/HomeController.cs
+++ IDT/Controllers/HomeController.cs
@@ -1,32 +1,45 @@
-using System.Diagnostics;
 using IDT.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
-namespace IDT.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
+    [HttpPost]
+    public IActionResult Login(string userid, string password)
+    {
+        if (userid == "IDT" && password == "Idt@1234")
         {
-            return View();
+            {
+                TempData["Success"] = "Logged in successfully!";
+                return RedirectToAction("HomePage", "Home");
+            }
         }
+        else
+        {
+            TempData["Error"] = "Invalid user ID or password";
+            return View("Index");
+        }
+    }
+    public IActionResult HomePage()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
